10c51435e0541770995c7cc6ffd02c75e01bb7ea
uriah_ah <109769619+ukaqm@users.noreply.github.com>
2024-04-05 14:29:28 -0600

    dynamicpagination

---
+++ ShoppingProject/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using ShoppingProject.Models.ViewModels;
+
+namespace ShoppingProject.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PaginationTagHelper : TagHelper
+    {
+        public IUrlHelperFactory UrlHelperFactory;
+
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            UrlHelperFactory = temp;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext? ViewContext { get; set; }
+        public string? PageAction { get; set; }
+        public PaginationInfo PageModel { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ViewContext != null && PageModel != null) 
+            {
+                IUrlHelper urlHelper = UrlHelperFactory.GetUrlHelper(ViewContext);
+
+                TagBuilder result = new TagBuilder("div");
+
+                for (int i = 1; i <= PageModel.TotalNumPages; i++)
+                {
+                    TagBuilder tag = new TagBuilder("a");
+
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
+                    tag.InnerHtml.Append(i.ToString());
+                    result.InnerHtml.AppendHtml(tag);
+                }
+                output.Content.AppendHtml(result.InnerHtml);
+            }
+        }
+    }
+}
